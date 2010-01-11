515017425dbf2c3e1ee914e64e67d1e537048fef
Jason Baker <amnorvend@gmail.com>
2010-01-11 16:30:52 -0600

    Adding a period.

--- scion/options.py
+++ scion/options.py
@@ -1,7 +1,7 @@
 def get_dis_printer(args):
     """
     Either get a real dis function or a dummy one depending on what
-    command-line options are specified
+    command-line options are specified.
     """
     if args.dis:
         from dis import dis as _dis
